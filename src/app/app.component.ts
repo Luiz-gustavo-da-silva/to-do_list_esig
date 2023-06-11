be274a3393985f3e9f52425e0985ab1314f1b14e
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2023-06-11 15:55:21 -0300

    feat: criação do filtro e tabela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component  } from '@angular/core';
-
+import { Component, ViewChild  } from '@angular/core';
+import { DialogComponent } from './dialog/dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import {  MatPaginator } from '@angular/material/paginator';
+import {  MatTableDataSource  } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,18 @@ import { Component  } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do_list_esig';
+  displayedColumns: string[] = ['titulo', 'responsavel', 'descricao', 'prioridade', 'deadline', 'action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private dialog : MatDialog){}
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '40%',
+    })
+  }
 
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import {MatIconModule} from '@angular/material/icon';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 
 @NgModule({
@@ -22,6 +30,17 @@ import { DialogComponent } from './dialog/dialog.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
